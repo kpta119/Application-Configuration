66eae6a3d726cfa032ecb21b0e4ce6722f74a084
Kacper Ptaszek <kacper.pt121@gmail.com>
2024-04-17 15:38:52 +0200

    dodanie metod i pól klasy KeyValuePair

--- ConfigurationLib/KeyValuePair.cpp
+++ ConfigurationLib/KeyValuePair.cpp
@@ -1,1 +1,23 @@
 #include "KeyValuePair.h"
+#include <string>
+
+KeyValuePair::KeyValuePair(std::string key, std::string value) {
+	this->key = key;
+	this->value = value;
+}
+
+std::string KeyValuePair::getKey() {
+	return key;
+}
+
+std::string KeyValuePair::getValue() {
+	return value;
+}
+
+void KeyValuePair::setValue(std::string value) {
+	this->value = value;
+}
+
+void KeyValuePair::setKey(std::string key) {
+	this->key = key;
+}
--- ConfigurationLib/KeyValuePair.h
+++ ConfigurationLib/KeyValuePair.h
@@ -1,5 +1,15 @@
-#pragma once
+#include <string>
 class KeyValuePair
 {
+private:
+	std::string key;
+	std::string value;
+public:
+	KeyValuePair(std::string key, std::string value);
+	~KeyValuePair();
+	std::string getKey();
+	std::string getValue();
+	void setKey(std::string key);
+	void setValue(std::string value);
 };
 
